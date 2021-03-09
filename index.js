b1e90abcb3c1ace33f9c332e04c62b35a798166f
erwanitwish <quillec.erwan@gmail.com>
2021-03-09 19:57:14 +0100

    création premières routes pour backoffice: ajout de lieu simple

--- index.js
+++ index.js
@@ -16,6 +16,47 @@ app.get("/", (request, response) => {
   response.send("Bienvenue sur mon API");
 });
 
+app.get("/description", (req, res) => {
+  connection.query("DESCRIBE lieu", (err, results) => {
+    if (err) {
+      res.status(500).json({
+        message: "Erreur lors de l'enregistrement de vos données",
+        error: err,
+      });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get("/lieux", (req, res) => {
+  connection.query("SELECT * FROM lieu", (err, results) => {
+    if (err) {
+      res.status(500).json({
+        message: "Erreur lors de l'enregistrement de vos données",
+        error: err,
+      });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.post("/lieu", (req, res) => {
+  const formData = req.body;
+  connection.query("INSERT INTO lieu SET ?", formData, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Erreur lors de l'ajout d'un projet",
+        error: err,
+      });
+    } else {
+      res.sendStatus(201);
+    }
+  });
+});
+
 app.listen(port, (err) => {
   if (err) {
     throw new Error("Something bad happened...");
